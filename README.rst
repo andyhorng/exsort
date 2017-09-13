b8a9330b966940665db98d7876cefd89a69df2e1
Andy <andyhorng168@gmail.com>
2017-09-13 23:38:18 +0800

    Project init

---
+++ README.rst
@@ -0,0 +1,24 @@
+exsort
+======
+
+-----
+
+.. contents:: **Table of Contents**
+    :backlinks: none
+
+Installation
+------------
+
+exsort is distributed on `PyPI <https://pypi.org>`_ as a universal
+wheel and is available on Linux/macOS and Windows and supports
+Python 2.7/3.5+ and PyPy.
+
+.. code-block:: bash
+
+    $ pip install exsort
+
+License
+-------
+
+exsort is distributed under the terms of the
+`MIT License <https://choosealicense.com/licenses/mit>`_.
---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import find_packages, setup
+
+with open('exsort/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.rst', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+REQUIRES = []
+
+setup(
+    name='exsort',
+    version=version,
+    description='',
+    long_description=readme,
+    author='Andy Hung',
+    author_email='andyhorng168@gmail.com',
+    maintainer='Andy Hung',
+    maintainer_email='andyhorng168@gmail.com',
+    url='https://github.com/andyhorng/exsort',
+    license='MIT',
+
+    keywords=[
+        '',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
+)
