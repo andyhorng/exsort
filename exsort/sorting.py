6426005c4293e9936ef04b6a7ca3fd8db1cb753c
Andy <andyhorng168@gmail.com>
2017-09-16 13:11:12 +0800

    V1

--- exsort/sorting.py
+++ exsort/sorting.py
@@ -1,7 +1,30 @@
 import heapq
-import contextlib
+import tempfile
+import itertools
+import pickle
 
 
-@contextlib.contextmanager
-def exsort(iterable, key=None, reverse=False):
-    yield sorted(iterable, key=key)
+def exsort(iterable, key=None, reverse=False, chunk_size=2**16):
+    # divide the iterable and dump into temp file as pickle
+    sorted_chunks = []
+    while True:
+        iterator = itertools.islice(iterable, chunk_size)
+        chunk = tempfile.TemporaryFile()
+
+        for item in sorted(iterator, key=key, reverse=reverse):
+            pickle.dump(item, chunk)
+
+        if chunk.tell() > 0:
+            chunk.seek(0)
+            sorted_chunks.append(chunk)
+        else:
+            break
+
+    def load(chunk):
+        while True:
+            try:
+                yield pickle.load(chunk)
+            except EOFError:
+                break
+
+    return heapq.merge(*map(load, sorted_chunks), key=key, reverse=reverse)
--- tests/test_exsort.py
+++ tests/test_exsort.py
@@ -10,9 +10,8 @@ def test_exsort():
     def items():
         while True:
             a = random.choice(string.ascii_letters)
-            yield (a, random.randint(0, 2**32))
+            yield (a, random.randint(0, 20))
 
-    with exsort(itertools.islice(items(), 10000), key=operator.itemgetter(1)) as sorted_items:
-        assert isinstance(sorted_items, list)
-        for i in range(len(sorted_items) - 1):
-            assert sorted_items[i][1] <= sorted_items[i+1][1]
+    sorted_items = list(exsort(itertools.islice(items(), 10), key=operator.itemgetter(1), chunk_size=2))
+    for i in range(len(sorted_items) - 1):
+        assert sorted_items[i][1] <= sorted_items[i+1][1]
